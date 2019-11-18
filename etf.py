b821956ce91d0e6c861b8f11a8a5ea42d077b2db
Gatien <gatien.gaumerais@gmail.com>
2019-11-18 09:56:55 +0100

    Added first functions necessary for the etf

--- etf.py
+++ etf.py
@@ -1,8 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 
-def etf
+Image.open('suza.jpg', mode='RGB')
 
+# EFT function
+def etf(kernel, image):
+    image.convert('LA')
+    
+    height, width = image.shape
+    
+    for i in range(0, height):
+        for j in range(0, width):
+            image[i,j]=etfIter(i, j, kernel)
 
-return 0
+# One iteration of ETF
+def etfIter(i, j, kernel):
+    
+    sum = 0
+    numrows = len(kernel)    # 3 rows in your example
+    numcols = len(kernel[0]) # 2 columns in your example
+    k = numcols * numrows
+    return 1,1
+
+# Normalised gradient magnitude
+def normalizedGrad():
+    print("coucou2")
+    return 0
