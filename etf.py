7df0b0f4e8aaebdb6339dc3b11a574ffb157701d
Gatien <gatien.gaumerais@gmail.com>
2020-01-27 09:49:52 +0100

    Improved code layout

--- etf.py
+++ etf.py
@@ -30,7 +30,7 @@ def computeWd(x, y):
         return abs(np.dot(x/np.linalg.norm(x),y/np.linalg.norm(y)))
 
 # EFT function
-def etf(img, kernel_size, img_grad, img_t):
+def etfIter(img, kernel_size, img_grad, img_t):
 
     # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     height, width = img.shape
@@ -38,12 +38,12 @@ def etf(img, kernel_size, img_grad, img_t):
     
     for i in range(len(img_t_res)):
         for j in range(len(img_t_res[0])):
-            img_t_res[i][j] = etfIter(i, j, kernel_size, img_t, img_grad)
+            img_t_res[i][j] = etfKernel(i, j, kernel_size, img_t, img_grad)
             # print("sum: ",img_t_res[i][j])
     return img_t_res
 
 # One iteration of ETF
-def etfIter(x_a, x_b, kernel, img_t, img_grad):
+def etfKernel(x_a, x_b, kernel, img_t, img_grad):
     
     sum = [0,0]
     k = kernel**2
@@ -85,38 +85,42 @@ def displayImgT(img_t, text, block):
 
     max_val = np.max(img_res_norm)
     print(text+" size :", len(img_res_norm), " - ", len(img_res_norm[0]))
-    # print(img_res_norm)
+    
     #cv2.imwrite("ETF_normalized.jpg", img_res_norm/max_val * 255)
     cv2.imshow(text, img_res_norm/max_val)
     if block : cv2.waitKey(0)
+
+def etf(img_src, nb_iter, kernel_size, display):
+    height, width = img_src.shape
+
+    # Solbel filter to get the gradient
+    sobel_x = cv2.Sobel(img_src,cv2.CV_64F,1,0,ksize=5)
+    sobel_y = cv2.Sobel(img_src,cv2.CV_64F,0,1,ksize=5)
+
+    img_grad = np.hypot(sobel_x, sobel_y) # Take the magnitude of the gradient
+    img_grad = cv2.normalize(img_grad.astype('float32'), None, 0.0, 1.0, cv2.NORM_MINMAX) # Normalize the gradient magnitude
+
+    img_t = [[[0,0] for x in range(width)] for y in range(height)] # t(y)
+
+    for i in range(len(img)):
+        for j in range(len(img[i])):
+            img_t[i][j] = [sobel_y[i][j], -sobel_x[i][j]]
+
+    if display : displayImgT(img_t, "Img t before", False)
+
+    for i in range(0,3):
+        img_t = etfIter(img, kernel_size, img_grad, img_t)
+
+    img_res = img_t
+
+    if display:
+        print("ETF not normalized:", len(img_res), " - ", len(img_res[0]))
+        displayImgT(img_res, "Img res", True)
+
+    return img_res
 
 # Main
 img = cv2.imread("images/baboon.jpg",0)
 height, width = img.shape
 
-# Solbel filter to get the gradient
-sobel_x = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-sobel_y = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
-
-img_grad = np.hypot(sobel_x, sobel_y) # Take the magnitude of the gradient
-img_grad = cv2.normalize(img_grad.astype('float32'), None, 0.0, 1.0, cv2.NORM_MINMAX) # Normalize the gradient magnitude
-
-img_t = [[[0,0] for x in range(width)] for y in range(height)] # t(y)
-for i in range(len(img)):
-        for j in range(len(img[i])):
-            img_t[i][j] = [sobel_y[i][j], -sobel_x[i][j]]
-
-displayImgT(img_t, "Img t before", False)
-
-kernel_size = 3
-
-for i in range(0,3):
-    img_t = etf(img, kernel_size, img_grad, img_t)
-
-img_res = img_t
-
-print("ETF not normalized:", len(img_res), " - ", len(img_res[0]))
-# cv2.imshow("ETF not normalized",img_res)
-
-# Display
-displayImgT(img_res, "Img res", True)
+etf(img, 3, 3, True)
