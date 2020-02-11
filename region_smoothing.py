88199cd8cfc241dddd6e282c05aeb1942033bcf1
antoinefraboul <antoine.fraboul@protonmail.com>
2020-02-11 13:22:26 +0100

    last version 11/02

--- region_smoothing.py
+++ region_smoothing.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as mp
 import numpy as np
+import cv2
+from sklearn import preprocessing
 
 sigma_e = 2
 r_e = 50
@@ -22,57 +24,27 @@ def similarityWeight(x,y, sigma) :
     val = abs(x-y)
     return gaussian(val,0,sigma)
 
-# gaussian funtion 
-def Ce(img, etf_x, etf_y) :
-    height, width, __ = img.shape
-    res = np.zeros(height, width)
+def Ce_bis(img, etf_x, etf_y) :
+    size = img.shape
+    res = np.zeros(size)
+    kernel_lenght = 1
+    img[0,0] = 0
 
-    for i in range(height) :
-        for j in range(width) :
+    for i in range(size[0]) :
+        for j in range(size[1]) :
+            ce = 0
             norm = 0
-            val = 0
+            for  k in range(-kernel_lenght, kernel_lenght+ 1) :
+                vec = [( etf_y[i,j] , -etf_x[i,j])]
+                normalized_lx = preprocessing.normalize(vec, norm='l1')
+                lx = (i, j) + k * normalized_lx
+                t =  (i, j) + k * normalized_lx 
+                if  ((lx[0] >= 0) and (lx[1] >= 0) and (lx[0] <= (size[0]-1 ))and (lx[1] <= (size[0]-1)) ):
+                    norm_val = spatialWeight( img[  lx[0] , lx[1]] ) * similarityWeight( img[ i , j ] ,  img [ lx[0] , lx[1] ] , r_g )
+                    ce +=  img[ lx[0] ,  lx[1] ] * norm_val
+            norm += norm_val
 
-            x = y = y_neg = (i,j)
-            norm_mode = True
-
-            while (norm_mode) :
-                mode_pos = mode_neg = False
-
-                y = (round(y[0] + etf_x[y[0],y[1]]), round(y[1] + etf_y[y[0],y[1]]))
-                if(kernel_distance(x,y,alpha)) :
-                    norm += spatialWeight(img[i,j], sigma_e) * similarityWeight(img[i,j], img[i+y[0],j+y[1]], sigma_g)
-                    mode_pos = True
-
-                y_neg = (round(y[0] - etf_x[y_neg[0],y_neg[1]]), round(y[0] - etf_y[y_neg[0],y_neg[1]]))
-                if(kernel_distance(x,y_neg,alpha)) :
-                    norm += spatialWeight(img[i,j], sigma_e) * similarityWeight(img[i,j], img[i+s,j+s], sigma_g)
-                    mode_pos = True
-
-                norm_mode = mode_neg | mode_pos
-
-            y = y_neg = (i,j)
-            in_kernel = True
-
-            while (in_kernel) :
-                kernel_pos = kernel_neg = False
-
-                y = (round(y[0] + etf_x[y[0],y[1]]), round(y[1] + etf_y[y[0],y[1]]))
-                if(kernel_distance(x,y,alpha)) :
-                    val_unnorm= img[y[0], y[1]] * spatialWeight(img[i,j], sigma_e) * similarityWeight(img[i,j], img[y[0],y[1]+j], sigma_g) 
-                    val = val_unnorm / norm
-                    kernel_pos = True
-
-                y_neg = (round(y_neg[0] - etf_x[y_neg[0],y_neg[1]]), round(y_neg[1] - etf_y[y_neg[0],y_neg[1]]))
-                if(kernel_distance(x,y_neg,alpha)) :
-                    val_unnorm = img[i+s, j+s] * spatialWeight(img[i,j], sigma_e) * similarityWeight(img[i,j], img[i+s,y+j], sigma_g) 
-                    val = val_unnorm / norm
-                    kernel_neg = True
-
-                in_kernel = kernel_pos | kernel_neg
-
-            res[i,j] = val
-            
-    return res
+            res[i,j] = ce / norm
 
 def Cv(img, etf_x, etf_y) :
     height, width, __ = img.shape
@@ -128,7 +100,7 @@ def Cv(img, etf_x, etf_y) :
 def filtering(img, etf_x, etf_y) :
 
     for __ in range(5) :
-        img = Ce(img, etf_x, etf_y)
+        img = Ce_bis(img, etf_x, etf_y)
         img = Cv(img, etf_x, etf_y)
 
     return img
